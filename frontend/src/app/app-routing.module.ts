5aeb6878eac8bb80c9ee86eb7d1bee0275551b45
philipe <philipegama97@gmail.com>
2022-01-08 16:40:19 -0400

    create report and login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { SimulatorComponent } from './pages/simulator/simulator.component';
 
 const routes: Routes = [
   {
     path: 'simulator',
     component: SimulatorComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,19 +11,27 @@ import { SimulatorComponent } from './pages/simulator/simulator.component';
 import { ChartBarComponent } from './components/chart-bar/chart-bar.component';
 
 
-//external
-import { NgApexchartsModule } from 'ng-apexcharts';
+//material 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+
+import { NgApexchartsModule } from 'ng-apexcharts';
+
+import { LoginComponent } from './pages/login/login.component';
+import { SimulationReportComponent } from './components/simulation-report/simulation-report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimulatorComponent,
-    ChartBarComponent
+    ChartBarComponent,
+    LoginComponent,
+    SimulationReportComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +42,9 @@ import { MatSliderModule } from '@angular/material/slider';
     MatFormFieldModule,
     MatInputModule,
     MatSliderModule,
-    MatSelectModule
+    MatSelectModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
