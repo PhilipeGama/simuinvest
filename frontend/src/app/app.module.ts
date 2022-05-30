16ce82532e3099d1736398c24a1c2dce8f3505f6
philipe <philipegama97@gmail.com>
2022-05-30 10:53:58 -0400

    create material module and move material imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,53 +1,29 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CurrencyMaskModule } from "ng2-currency-mask";
-
 import { NgxMaskModule, IConfig } from 'ngx-mask'
 export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SimulatorComponent } from './pages/simulator/simulator.component';
-import { ChartBarComponent } from './components/chart-bar/chart-bar.component';
-
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
-
-//material 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-
-
-
-import {
-  MatSidenavModule
-} from '@angular/material/sidenav';
-
 import { NgApexchartsModule } from 'ng-apexcharts';
 
+import { SimulatorComponent } from './pages/simulator/simulator.component';
+import { ChartBarComponent } from './components/chart-bar/chart-bar.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SimulationReportComponent } from './components/simulation-report/simulation-report.component';
 import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
 import { HeaderComponent } from './components/templates/header/header.component';
 import { LoggedComponent } from './pages/logged/logged.component';
 import { InvestorProfileComponent } from './pages/investor-profile/investor-profile.component';
-
 import { HomeComponent } from './pages/home/home.component';
 import { LoadingSpinner } from './components/templates/loading-spinner/loading-spinner';
 import { TutorialComponent } from './tutorial/tutorial.component';
@@ -83,25 +59,12 @@ import { DialogConfirmComponent } from './components/dialog-confirm/dialog-confi
     AngularFireModule.initializeApp(environment.firebase),
     NgxMaskModule.forRoot(),
     AngularFireDatabaseModule,
-
+    MaterialModule,
     CurrencyMaskModule,
     FlexLayoutModule,
     NgApexchartsModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSliderModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatTableModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatMenuModule,
-    MatRadioModule,
-    MatCardModule,
-    MatSnackBarModule,
-    MatDialogModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/material.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+const MaterialComponents = [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSliderModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatRadioModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatDialogModule
+]
+
+@NgModule({
+    imports: [MaterialComponents],
+    exports: [MaterialComponents],
+})
+export class MaterialModule { }
